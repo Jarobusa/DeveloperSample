74abe072a05c2659f6027fee65bfaa0b067e2a9a
robbie <jarobusa@hotmail.com>
2024-01-08 15:11:13 -0500

    Container Example

--- DeveloperSample/Container.cs
+++ DeveloperSample/Container.cs
@@ -1,9 +1,43 @@
-using System;
-
 namespace DeveloperSample.Container;
 
-public class Container
+
+/// <summary>
+/// A simple container for binding interfaces to implementations
+/// </summary>
+public class Container : List<Type>
 {
-    public void Bind(Type interfaceType, Type implementationType) => throw new NotImplementedException();
-    public T Get<T>() => throw new NotImplementedException();
+    Dictionary<Type, Type> ImplementationsByType = new();
+
+    /// <summary>
+    /// Bind an interface to an implementation
+    /// </summary>
+    /// <param name="interfaceType"></param>
+    /// <param name="implementationType"></param>
+    public void Bind(Type interfaceType, Type implementationType)
+    {
+        ImplementationsByType[interfaceType] = implementationType;
+    }
+
+
+    /// <summary>
+    /// Get an instance of T
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    /// <exception cref="Exception"></exception>
+    public T Get<T>()
+    {
+        var type = typeof(T);
+        if (ImplementationsByType.ContainsKey(type))
+        {
+            type = ImplementationsByType[type];
+        }
+
+        var instance = Activator.CreateInstance(type);
+
+        if (instance is null)
+            throw new Exception("Instance of T is not found");
+
+        return (T)instance!;
+    }
 }
